763409826030fde59c397b363a05d44f3a3b1ede
Txddyt1 <igorschuller40@gmail.com>
2025-06-03 14:21:58 -0300

    feat: Entity Usuario e Entity Acesso

---
+++ backend/src/main/java/com/servitask/servitask/entity/Usuario.java
@@ -0,0 +1,67 @@
+package com.servitask.servitask.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+    private String nome;
+    @Column(unique = true, nullable = false)
+    private String email;
+
+
+    @OneToOne(mappedBy = "usuario", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Acesso acesso;
+
+
+    public usuario() {
+    }
+    public usuario(String nome, String email, Acesso acesso) {
+        this.nome = nome;
+        this.email = email;
+        this.acesso = acesso;
+    }
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Acesso getAcesso() {
+        return acesso;
+    }
+    public void setAcesso(Acesso acesso) {
+        this.acesso = acesso;
+    }
+
+    
+    
+    
+}
