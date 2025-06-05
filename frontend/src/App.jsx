fdfbfce040528ce07229f110077d822010a1e490
w1ze55 <cacauezin@gmail.com>
2025-06-05 10:10:38 -0300

    feat: added insert cards and drag-and-drop cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import Sidebar from './components/Sidebar'
 import Kanban from './components/Kanban'
 import './App.css'
 
 function App() {
   return (
     <div className="app-container">
-      <Sidebar />
       <Kanban />
     </div>
   )
