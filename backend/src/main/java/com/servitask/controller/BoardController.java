b3aa66ce9e1b108aa5b07ad399dc769b9f96f8d1
CAUE FERREIRA PERES <caue.peres@alunos.sc.senac.br>
2025-06-09 20:35:02 -0300

    feat: Added (BoardController.java) and endpoint

--- backend/src/main/java/com/servitask/controller/BoardController.java
+++ backend/src/main/java/com/servitask/controller/BoardController.java
@@ -1,5 +1,24 @@
 package com.servitask.controller;
 
+import com.servitask.model.Board;
+import com.servitask.model.BoardColumn;
+import com.servitask.model.Card;
+import com.servitask.servitask.entity.User;
+import com.servitask.service.BoardService;
+import com.servitask.servitask.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/boards")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")         
+
 public class BoardController {
-    
+
 }
